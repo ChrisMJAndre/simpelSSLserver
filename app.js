ad4c98903fbdfd51a9752cb7f40958cbf46bd85d
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-11-02 15:43:19 +0100

    callback and certs

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const http = require("http");
+const https = require("http");
 const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
 
-const sslServer = https.createServer({ key, cert }, app);
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
 
 app.use("/", (req, res) => {
   res.send("Connection to server");
 });
 
-sslServer.listen(3443);
+sslServer.listen(3443, () => {
+  console.log("server is listening on 3443");
+});
